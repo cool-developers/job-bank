21d2ef98017544243d1382f1bb9cc705b5a2d56e
ik_2dw3 <ik_2dw3@H015110.NET>
2016-12-21 09:56:08 +0100

    Login

---
+++ application/controllers/Login_controller.php
@@ -0,0 +1,54 @@
+<?php
+class Login_controller extends CI_Controller {
+
+        public function __construct()
+        {
+                parent::__construct();
+                $this->load->model('Login_model');
+                $this->load->helper('url_helper');
+        }
+
+		public function index()
+		{
+			$this->load->helper('form');
+	    	$this->load->library('form_validation');
+						
+        	$data['title'] = 'Login de USUARIOS';		
+			
+			
+			$this->form_validation->set_rules('user', 'Usuario', 'required');
+	    	$this->form_validation->set_rules('password', 'Contraseña', 'required');
+			
+	    	if ($this->form_validation->run() === FALSE)
+	    	{				
+        		$this->load->view('templates/header', $data);
+        		$this->load->view('login');
+        		$this->load->view('templates/footer');
+			}
+			else {
+				$resultado = $this->user_model->get_user();
+				if(password_verify($this->input->post('password'),$resultado['password'])){
+					echo('coinciden');
+					
+			$usuario_data = array(
+		               'user' => $this->input->post('user'),
+		               'logueado' => TRUE
+		            );
+		            $this->session->set_userdata($usuario_data);
+					$this->session->set_userdata('user',$this->input->post('user'));
+					
+					
+					
+					redirect("dietas");
+				}else{
+					$data['error']='Usuario o password incorrecta';
+					$this->load->view('templates/header', $data);
+        			$this->load->view('login/login', $data);
+        			$this->load->view('templates/footer');
+				}					
+	    	 
+	    	 }
+		     
+		}		
+}
+?>
---
+++ application/views/login/login.php
@@ -0,0 +1,23 @@
+<h2><?php echo $title; ?></h2>
+
+<ul>
+	<?php
+	if (isset($error)){
+		echo("<li>$error</li>");	
+	}
+	echo validation_errors("<li>","</li>"); ?>
+</ul>	
+	
+
+<?php echo form_open('login'); ?>
+
+ 
+    <label for="user">Usuario</label>
+    <input type="input" name="user" /><br />
+
+    <label for="password">Password</label>
+    <input type="input" name="password" /><br />
+
+    <button name="submit" value="Logearse">Logearse</button>
+
+</form>
